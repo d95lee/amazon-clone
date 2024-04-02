16390aa4e5a765e6696624305a6dbab124c57bcc
David <euntek95@gmail.com>
2024-04-01 22:39:49 -0700

    create products table

---
+++ app/models/product.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  price       :bigint           not null
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Product < ApplicationRecord
+    validates :name, :price, presence: true
+    validates :name, length: { in: 1..100 } allow_nil: true
+    validates :price, length: { in: 1..8 } allow_nil: true
+
+    belongs_to :user
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_204515) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_044436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "price", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_products_on_name", unique: true
+    t.index ["price"], name: "index_products_on_price", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
