978394b3c705e9f7c4a7d3072b4b6489d1929bae
David <euntek95@gmail.com>
2024-05-04 17:39:10 -0700

    add categories for each seed

--- frontend/src/components/Product/ProductShow.jsx
+++ frontend/src/components/Product/ProductShow.jsx
@@ -100,6 +100,7 @@ if (!product) {
                                 })}
                                 <p className='show-product-review-total-text'>{currentProductArr.length} ratings</p>
                         </div>
+                                <p>{product.category}</p>
                         
                         <hr />
                         {product && <p id='product-price'><span className='price-text'>Price:</span> ${product.price}</p>}
