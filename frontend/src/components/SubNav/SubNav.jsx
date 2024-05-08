3b2569b3bc4df488682accc8fa876fd7dbb6c0bb
David <euntek95@gmail.com>
2024-05-07 21:51:43 -0700

    prevent scrolling on subnav bar

--- frontend/src/components/SubNav/SubNav.jsx
+++ frontend/src/components/SubNav/SubNav.jsx
@@ -7,16 +7,16 @@ const SubNav = () => {
     return (
         <>
             <div className='subnav-bar'>
-                <Link to={'/products'}><p id='text-deals'>Today's Deals</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Keep Shopping</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Livestreams</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Music</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Amazon Basics</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Gift Cards</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Buy Again</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Kindle Books</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Subscribe & Save</p></Link>
-                <Link to={'/products'}><p id='text-deals'>Sell</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Today's Deals</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Keep Shopping</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Livestreams</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Music</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Amazon Basics</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Gift Cards</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Buy Again</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Kindle Books</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Subscribe & Save</p></Link>
+                <Link to={'/products'}><p className='text-deals'>Sell</p></Link>
             </div>
         </>
     )
